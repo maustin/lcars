ec66e9b7e6be4674c70b5cdec698bbdec8ae7381
Mike Austin <mikeaustin@ga-sf>
2019-12-20 11:21:08 -0800

    added description to home page and resized logo

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,8 +3,10 @@ import React from 'react';
 class HomePage extends React.Component {
 	render() {
 		return (
-			<div>
-				<img src='https://vignette.wikia.nocookie.net/memoryalpha/images/b/b5/United_Federation_of_Planets_logo.svg/revision/latest?cb=20101031000529&path-prefix=en' alt=''/>
+			<div className='home-page'>
+				<span className='home-page-text'>Welcome to the LCARS Star Trek API</span>
+				<span className='home-page-text'>This API covers Star Trek eras of The Next Generation (TNG) to Deep Space 9 (DS9)</span>
+				<img className='home-page-image' src='https://vignette.wikia.nocookie.net/memoryalpha/images/b/b5/United_Federation_of_Planets_logo.svg/revision/latest?cb=20101031000529&path-prefix=en' alt=''/>
 			</div>
 		)
 	}
